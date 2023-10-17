1bc5fa906a23f81e7b951629b8ea673c3d2a98e4
Mariano Murcia Ortuño <marmurort@edu.gva.es>
2023-10-17 23:28:19 +0200

    Agregar archivos de proyecto.

---
+++ EdicionDatos/fmEdicion.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace EdicionDatos
+{
+    partial class fmEdicion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.laNombre = new System.Windows.Forms.Label();
+            this.laDireccion = new System.Windows.Forms.Label();
+            this.laEmail = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.laDni = new System.Windows.Forms.Label();
+            this.laCodigoPostal = new System.Windows.Forms.Label();
+            this.laTelefono = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.laLocalidad = new System.Windows.Forms.Label();
+            this.tbEuros = new System.Windows.Forms.TextBox();
+            this.tbDolares = new System.Windows.Forms.TextBox();
+            this.tbCoEuro = new System.Windows.Forms.TextBox();
+            this.tbCoDolar = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btCambiar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.laLocalidad);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.laTelefono);
+            this.panel1.Controls.Add(this.laCodigoPostal);
+            this.panel1.Controls.Add(this.laDni);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.laEmail);
+            this.panel1.Controls.Add(this.laDireccion);
+            this.panel1.Controls.Add(this.laNombre);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 275);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(565, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DATOS INTRODUCIDOS DESDE OTRO FORMULARIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Direccion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "email";
+            // 
+            // laNombre
+            // 
+            this.laNombre.AutoSize = true;
+            this.laNombre.Location = new System.Drawing.Point(170, 50);
+            this.laNombre.Name = "laNombre";
+            this.laNombre.Size = new System.Drawing.Size(16, 20);
+            this.laNombre.TabIndex = 4;
+            this.laNombre.Text = "x";
+            // 
+            // laDireccion
+            // 
+            this.laDireccion.AutoSize = true;
+            this.laDireccion.Location = new System.Drawing.Point(170, 85);
+            this.laDireccion.Name = "laDireccion";
+            this.laDireccion.Size = new System.Drawing.Size(16, 20);
+            this.laDireccion.TabIndex = 5;
+            this.laDireccion.Text = "x";
+            // 
+            // laEmail
+            // 
+            this.laEmail.AutoSize = true;
+            this.laEmail.Location = new System.Drawing.Point(170, 117);
+            this.laEmail.Name = "laEmail";
+            this.laEmail.Size = new System.Drawing.Size(16, 20);
+            this.laEmail.TabIndex = 6;
+            this.laEmail.Text = "x";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(399, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "DNI/CIF";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(399, 85);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Codigo Postal";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(399, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Telefono";
+            // 
+            // laDni
+            // 
+            this.laDni.AutoSize = true;
+            this.laDni.Location = new System.Drawing.Point(511, 56);
+            this.laDni.Name = "laDni";
+            this.laDni.Size = new System.Drawing.Size(16, 20);
+            this.laDni.TabIndex = 10;
+            this.laDni.Text = "x";
+            // 
+            // laCodigoPostal
+            // 
+            this.laCodigoPostal.AutoSize = true;
+            this.laCodigoPostal.Location = new System.Drawing.Point(511, 85);
+            this.laCodigoPostal.Name = "laCodigoPostal";
+            this.laCodigoPostal.Size = new System.Drawing.Size(16, 20);
+            this.laCodigoPostal.TabIndex = 11;
+            this.laCodigoPostal.Text = "x";
+            // 
+            // laTelefono
+            // 
+            this.laTelefono.AutoSize = true;
+            this.laTelefono.Location = new System.Drawing.Point(511, 117);
+            this.laTelefono.Name = "laTelefono";
+            this.laTelefono.Size = new System.Drawing.Size(16, 20);
+            this.laTelefono.TabIndex = 12;
+            this.laTelefono.Text = "x";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(614, 85);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(77, 20);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Localidad";
+            // 
+            // laLocalidad
+            // 
+            this.laLocalidad.AutoSize = true;
+            this.laLocalidad.Location = new System.Drawing.Point(703, 85);
+            this.laLocalidad.Name = "laLocalidad";
+            this.laLocalidad.Size = new System.Drawing.Size(16, 20);
+            this.laLocalidad.TabIndex = 14;
+            this.laLocalidad.Text = "x";
+            // 
+            // tbEuros
+            // 
+            this.tbEuros.Location = new System.Drawing.Point(90, 371);
+            this.tbEuros.Name = "tbEuros";
+            this.tbEuros.Size = new System.Drawing.Size(100, 20);
+            this.tbEuros.TabIndex = 1;
+            this.tbEuros.Click += new System.EventHandler(this.tbEuros_Click);
+            this.tbEuros.TextChanged += new System.EventHandler(this.tbEuros_TextChanged);
+            this.tbEuros.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbEuros_KeyPress);
+            // 
+            // tbDolares
+            // 
+            this.tbDolares.Location = new System.Drawing.Point(257, 371);
+            this.tbDolares.Name = "tbDolares";
+            this.tbDolares.Size = new System.Drawing.Size(100, 20);
+            this.tbDolares.TabIndex = 2;
+            this.tbDolares.Click += new System.EventHandler(this.tbEuros_Click);
+            this.tbDolares.TextChanged += new System.EventHandler(this.tbEuros_TextChanged);
+            this.tbDolares.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbEuros_KeyPress);
+            // 
+            // tbCoEuro
+            // 
+            this.tbCoEuro.Location = new System.Drawing.Point(259, 39);
+            this.tbCoEuro.Name = "tbCoEuro";
+            this.tbCoEuro.Size = new System.Drawing.Size(75, 20);
+            this.tbCoEuro.TabIndex = 3;
+            this.tbCoEuro.Text = "1,016";
+            this.tbCoEuro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbEuros_KeyPress);
+            // 
+            // tbCoDolar
+            // 
+            this.tbCoDolar.Location = new System.Drawing.Point(259, 77);
+            this.tbCoDolar.Name = "tbCoDolar";
+            this.tbCoDolar.Size = new System.Drawing.Size(75, 20);
+            this.tbCoDolar.TabIndex = 4;
+            this.tbCoDolar.Text = "0,984";
+            this.tbCoDolar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbEuros_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbCoDolar);
+            this.groupBox1.Controls.Add(this.tbCoEuro);
+            this.groupBox1.Location = new System.Drawing.Point(449, 296);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 115);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ULTIMA COTIZACION";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "1 Euro en Dólar estadounidense es igual a";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(248, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "1 Dólar estadounidense en Euro es igual a";
+            // 
+            // btCambiar
+            // 
+            this.btCambiar.Location = new System.Drawing.Point(186, 397);
+            this.btCambiar.Name = "btCambiar";
+            this.btCambiar.Size = new System.Drawing.Size(75, 23);
+            this.btCambiar.TabIndex = 4;
+            this.btCambiar.Text = "Cambiar";
+            this.btCambiar.UseVisualStyleBackColor = true;
+            this.btCambiar.Click += new System.EventHandler(this.btCambiar_Click);
+            // 
+            // fmEdicion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCambiar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbDolares);
+            this.Controls.Add(this.tbEuros);
+            this.Controls.Add(this.panel1);
+            this.Name = "fmEdicion";
+            this.Text = "Edicion Datos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label laTelefono;
+        private System.Windows.Forms.Label laCodigoPostal;
+        private System.Windows.Forms.Label laDni;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label laEmail;
+        private System.Windows.Forms.Label laDireccion;
+        private System.Windows.Forms.Label laNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label laLocalidad;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox tbEuros;
+        private System.Windows.Forms.TextBox tbDolares;
+        private System.Windows.Forms.TextBox tbCoEuro;
+        private System.Windows.Forms.TextBox tbCoDolar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btCambiar;
+    }
+}
+
---
+++ EdicionDatos/fmEdicion.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EdicionDatos
+{
+    public partial class fmEdicion : Form
+    {
+        public fmEdicion()
+        {
+            InitializeComponent();
+        }
+
+        private void btCambiar_Click(object sender, EventArgs e)
+        {
+            Single total, formatea; //Variables para cálculos
+            try
+            {
+                if (Convert.ToSingle(tbDolares.Text) > 0)
+                { // Convertimos de dólares a Euros
+                    formatea = Convert.ToSingle(tbDolares.Text);
+                    total = Convert.ToSingle(tbDolares.Text) *
+                    Convert.ToSingle(tbCoEuro.Text);
+                    // Formateo numérico
+                    tbEuros.Text = total.ToString("#,###,##0.00");
+                    tbDolares.Text = formatea.ToString("#,###,##0.00");
+                }
+                else
+                { // Convertimos de Euros a Dólares
+                    formatea = Convert.ToSingle(tbEuros.Text);
+                    total = Convert.ToSingle(tbEuros.Text) *
+                    Convert.ToSingle(tbCoDolar.Text);
+                    tbDolares.Text = total.ToString("#,###,##0.00");
+                    tbEuros.Text = formatea.ToString("#,###,##0.00");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+        private void tbEuros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                btCambiar.PerformClick();
+            }
+            switch (e.KeyChar)
+            {
+                case (char)8:
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                case ',': break;
+                default:
+                    e.KeyChar = (char)0; //anulamos la pulsación
+                    break;
+            }
+        }
+
+        private void tbEuros_TextChanged(object sender, EventArgs e)
+        {
+            btCambiar.Enabled = true;
+        }
+
+        private void tbEuros_Click(object sender, EventArgs e)
+        {
+            tbDolares.Text = "0"; // Iniciamos para nuevo cálculo
+            tbEuros.Text = "0";
+            TextBox mitextbox = (TextBox)sender; // Seleccionamos
+            mitextbox.SelectAll(); // para que al teclear se borre todo
+        }
+    }
+}
